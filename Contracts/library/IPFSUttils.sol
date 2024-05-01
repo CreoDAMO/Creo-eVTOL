9e895dd2ea1030dd4c83610c02e52c68cb182fe5
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-05-01 19:54:57 -0400

    Update IPFSUttils.sol

--- Contracts/library/IPFSUttils.sol
+++ Contracts/library/IPFSUttils.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+library IPFSUtils {
+    // IPFS client connection and imports would be here
+    
+    /**
+     * @dev Store data on IPFS and return the Content Identifier (CID)
+     * @param data The data to store on IPFS
+     * @return cid The Content Identifier (CID) of the stored data
+     */
+    function storeData(string memory data) internal returns (bytes32 cid) {
+        // Implement the logic to store data on IPFS
+        // Pin the data and obtain the CID (Content Identifier)
+        // Return the CID as bytes32
+    }
+    
+    /**
+     * @dev Retrieve data from IPFS using the Content Identifier (CID)
+     * @param cid The Content Identifier (CID) of the data on IPFS
+     * @return data The retrieved data from IPFS
+     */
+    function retrieveData(bytes32 cid) internal view returns (string memory data) {
+        // Implement the logic to retrieve data from IPFS using CID
+        // Return the retrieved data as a string
+    }
+    
+    // Other utility functions for IPFS handling
+}
